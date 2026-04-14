3f3c3448d4866228ddcad30ef879b60d808c4264
JoseManuelZarcoGarcia <jose.manuel.zarco.garcia@gmail.com>
2026-04-14 14:28:32 +0200

    implementacion inicial de Ahorro 2

--- src/application/AhorroController.java
+++ src/application/AhorroController.java
@@ -1,5 +1,6 @@
 package application;
 
+import entidades.Categorias;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
@@ -39,9 +40,12 @@ public class AhorroController {
 			String frecuenciaPago = comboFrecuenciaPago.getValue();
 
 			int cantidadAhorro = Integer.parseInt(txtCantidadAhorro.getText());
-
+			
+			entidades.Categorias cA = new Categorias(nombreCategoria, false, cantidadAhorro, cantidadAhorro, descripcionAhorro, frecuenciaPago);
+			
 			servicio.crearAhorro(frecuenciaPago, nombreCategoria, cantidadAhorro, descripcionAhorro);
-
+			
+			 System.out.println(cA.toString());
 			limpiarCampos();
 			cerrarVentana(); // no todos los cambios realizados, solo cerrarVentana
 
